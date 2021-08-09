0efe4e2235ba11c1047a11d981d6bb545ab6d2b2
Geoffreydoco <81360901+Geoffreydoco@users.noreply.github.com>
2021-08-09 15:59:32 +0200

    Update hangman.js
    a  bit more features

--- pendu/hangman.js
+++ pendu/hangman.js
@@ -1,50 +1,59 @@
 //generating random word
 var wordsarray = ['apple', 'front', 'store', 'magic', 'paris', 'virus', 'drone', 'glove', 'where', 'salad'];
-const wordtofind = wordsarray[Math.floor(Math.random() * 10)];
+const wordtofind = wordsarray[Math.floor(Math.random() * wordsarray.length)];
 console.log(wordtofind)
 //objects declaration
 const button = document.querySelector('#btn')
 const input = document.getElementById("input")
 let playersletter = ''
-let letter = 'letter'
 let wincondition = 0
 //Display trials
 let nbrtrial = 8
 const trial = document.querySelector('.trial')
-trial.innerHTML = "Remaining trial(s) : ".concat(nbrtrial)
+trial.innerHTML = "Remaining trial(s) : " + nbrtrial
 //getting input from player
 input.addEventListener('input', (e) => {
   playersletter = (e.target.value).toLowerCase()
 })
 //Trial Main function
 button.addEventListener('click', () => {
-  for (i = 0; i < wordtofind.length; i++) {
-    //comparing player input
-    if (playersletter == wordtofind[i]) {
-      const letterdom = document.querySelector("#".concat('letter', i, ""))
-      // Dom filling good answer and wincondition stepping
-      if (letterdom.innerHTML == "_") {
-        letterdom.innerHTML = playersletter
-        wincondition = wincondition + 1
+  if (playersletter) {
+    for (i = 0; i < wordtofind.length; i++) {
+      //comparing player input
+      if (playersletter == wordtofind[i]) {
+        const letterdom = document.querySelector("#".concat('letter', i))
+        // Dom filling good answer and wincondition stepping
+        if (letterdom.innerHTML == "_") {
+          letterdom.innerHTML = playersletter
+          wincondition = wincondition + 1
+        }
       }
     }
+    //countdown and losing condition
+    nbrtrial = nbrtrial - 1
+    trial.innerHTML = "Remaining trial(s) : " + nbrtrial
+    if (nbrtrial == 0 && wincondition !== 5) {
+      alert("No attempts remaining try again !")
+      document.location.reload();
+    }
+    //Ending setup
+    if (wincondition == 5) {
+      alert("You're the boss. The Mysterious word was " + wordtofind.toUpperCase() + ". Congratulation !")
+      document.location.reload();
+    }
+    // Clear value and autofocus
+    playersletter = "";
+    input.value = "";
+    input.focus();
   }
-  //countdown and losing condition
-  nbrtrial = nbrtrial - 1
-  trial.innerHTML = "Remaining trial(s) : ".concat(nbrtrial)
-  if (nbrtrial == 0) {
-    alert("No attempts remaining try again !")
-    document.location.reload();
-  }
-  //Ending setup
-  if (wincondition == 5) {
-    alert("You're the boss. The Mysterious word was "+wordtofind.toUpperCase()+". Congratulation !")
-    document.location.reload();
-  }
-  // Clear value and autofocus
-  input.value = "";
-  input.focus();
 })
+//Alternate validation enter key
+input.addEventListener("keyup", function(e) {
+   if (e.code === 'Enter') {
+      e.preventDefault();
+      button.click();
+   }
+});
 
 
 
