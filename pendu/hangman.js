4bea9a8412056699a18fecbec8bc67bb70752fdb
Geoffreydoco <81360901+Geoffreydoco@users.noreply.github.com>
2021-08-09 14:22:13 +0200

    Update hangman.js
    V1 done

--- pendu/hangman.js
+++ pendu/hangman.js
@@ -1,29 +1,31 @@
-var wordsarray = ['apple', 'front', 'store', 'magic', 'paris', 'virus', 'drone', 'glove', 'where', 'salad'];
 //generating random word
+var wordsarray = ['apple', 'front', 'store', 'magic', 'paris', 'virus', 'drone', 'glove', 'where', 'salad'];
 const wordtofind = wordsarray[Math.floor(Math.random() * 10)];
 console.log(wordtofind)
+//objects declaration
 const button = document.querySelector('#btn')
-const trial = document.querySelector('.trial')
 const input = document.getElementById("input")
-let nbrtrial = 8
-trial.innerHTML = "Remaining trial(s) : ".concat(nbrtrial)
-let playersletter =''
+let playersletter = ''
 let letter = 'letter'
 let wincondition = 0
-
+//Display trials
+let nbrtrial = 8
+const trial = document.querySelector('.trial')
+trial.innerHTML = "Remaining trial(s) : ".concat(nbrtrial)
+//getting input from player
+input.addEventListener('input', (e) => {
+  playersletter = (e.target.value).toLowerCase()
+})
+//Trial Main function
 button.addEventListener('click', () => {
-  //getting input from player
-  document.getElementById('input').addEventListener('input', (e) => {
-    playersletter = (e.target.value).toLowerCase()
-  })
   for (i = 0; i < wordtofind.length; i++) {
     //comparing player input
     if (playersletter == wordtofind[i]) {
       const letterdom = document.querySelector("#".concat('letter', i, ""))
       // Dom filling good answer and wincondition stepping
-      if (letterdom.innerHTML == "_"){
-      letterdom.innerHTML = playersletter      
-      wincondition = wincondition + 1
+      if (letterdom.innerHTML == "_") {
+        letterdom.innerHTML = playersletter
+        wincondition = wincondition + 1
       }
     }
   }
@@ -36,8 +38,8 @@ button.addEventListener('click', () => {
   }
   //Ending setup
   if (wincondition == 5) {
-    alert("You found the Mysterious word. Congratulation !") 
-    document.location.reload();   
+    alert("You're the boss. The Mysterious word was "+wordtofind.toUpperCase()+". Congratulation !")
+    document.location.reload();
   }
   // Clear value and autofocus
   input.value = "";
